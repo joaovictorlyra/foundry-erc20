5d51c951217d71a86f53cfadc10709947af41a06
joao-lyra <jvmlyra@gmail.com>
2025-08-26 11:32:29 -0300

    fix fmt

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 import "forge-std/Script.sol";
 import "../src/OurToken.sol";
 
-contract DeployOurToken is Script{
+contract DeployOurToken is Script {
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
 
     function run() external returns (OurToken) {
